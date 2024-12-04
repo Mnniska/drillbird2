fd054b7854b8216a07d048b28315cde4c7e29302
Mnniska <slebbastian@gmail.com>
2024-12-04 21:29:25 +0100

    worked on inventory stuff

---
+++ drillbird/Scripts/abstract_ore.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name abstract_ore
+
+@export var value:int 
+@export var sprite:Texture
+@export var name = "default"
+@export var stackSize:int
--- drillbird/Scripts/ui_inventory_slot.gd
+++ drillbird/Scripts/ui_inventory_slot.gd
@@ -6,11 +6,32 @@ The InventorySlot keeps track of the ores it has and updates itself visually
 var oreTex:Texture2D
 @onready var current = $current
 @onready var max = $max
+var new_ore: abstract_ore
+var chosen_ore: abstract_ore
 var maxItems:int =3
 var currentItems: int =0
+var occupied:bool = false
+
+func GiveOre(_newore:abstract_ore):
+	new_ore =_newore
+	
+	if(currentItems==0):
+		chosen_ore=new_ore
+			
+	if(maxItems>currentItems):
+		
+		currentItems+=1
+		
+		
+	
+	pass
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	chosen_ore=null
+	
 	pass # Replace with function body.
 
 
