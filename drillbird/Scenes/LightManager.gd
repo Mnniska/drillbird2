0387e215d071c0d003773e42773d2b8a6bb6399f
Mnniska <slebbastian@gmail.com>
2024-12-09 12:08:43 +0100

    Made lightbulbs work

--- drillbird/Scenes/LightManager.gd
+++ drillbird/Scenes/LightManager.gd
@@ -3,12 +3,13 @@ extends Node2D
 #this will be given on load game, updated via the SHOP as well 
 @export var bulbAmount:int
 
+@export var lightUpgrade:int =2
+var lightBulbArray:Array[Sprite2D]
+
 #Time variables
-@export var time_TimerLength:int=10
+@export var time_TimerLength:int=60
 var time_Countdown:float
-
-
-
+var darknessClose:bool=false
 var maxSize:int=75
 var minSize:int=1
 var internal_upd_interval:int=60
@@ -17,11 +18,46 @@ var internal_upd_counter:int=0
 @onready var PlayerLight=$"../../Player/PlayerDarkness"
 @onready var LightSlider=$UI_LightSlider
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	time_Countdown=time_TimerLength
-	pass # Replace with function body.
 	
+	for n in lightUpgrade:
+		var scene = load("res://Scenes/lightbulb.tscn") 
+		var node = scene.instantiate()
+		node.SetActive(true)
+		lightBulbArray.append(node)
+		
+		var offset= Vector2(0,-4)
+		
+		node.transform.origin = offset
+		add_child(node)
+	UpdateLightbulbLocations()
+
+func UpdateLightbulbLocations():
+	var offset:int=0
+	offset=(floor(lightBulbArray.size())/2)*8
+	offset-=4 #accounts for the pivot not being at the side
+	if(lightBulbArray.size()%2>0):
+		offset+=4
+		pass
+	var index:int =0 
+	for n in lightBulbArray:
+		n.position.x=self.position.x+ offset-(8*index)
+		index+=1
+	
+func GetNextLightbulb():
+	for n in lightBulbArray:
+		if n.active:
+			n.SetActive(false)
+			time_Countdown=time_TimerLength
+			break
+	
+	for n in lightBulbArray:
+		if n.active:
+			return true
+	darknessClose=true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -32,17 +68,26 @@ func _process(delta: float) -> void:
 		internal_upd_counter+=1
 		if internal_upd_counter>internal_upd_interval:
 			internal_upd_counter=0
+			
 			var progress=float(time_Countdown/time_TimerLength)
 			LightSlider.value=progress*100
-			PlayerLight.SetLight(progress)
 			
-		
+			if darknessClose:
+				PlayerLight.SetLight(progress)
+			
+	else:
+		GetNextLightbulb()
 	pass
 	
 func handleInputs():
 
 	if Input.is_action_just_pressed('addLight'):
+		for n in lightBulbArray:
+			n.SetActive(true)
 		time_Countdown=time_TimerLength
+		darknessClose=false
+		PlayerLight.SetLight(1)
+
 		pass
 
 	if Input.is_action_just_pressed('removeLight'):
---
+++ drillbird/lightbulb_script.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+@export var tex_active :Texture2D
+@export var tex_off :Texture2D
+var active:bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func SetActive(_active:bool):
+	active=_active
+	if(active):
+		texture = tex_active
+	else:
+		texture = tex_off
