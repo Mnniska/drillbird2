1ecbdea4c0a910fdea944793ca5b1949dc6d16fd
Mnniska <slebbastian@gmail.com>
2024-12-12 15:37:40 +0100

    Added torches to HUB & revised layour
    - Figured out how to support multiple light sources. Should be able to dynamically add light sources when drilling, seems quite flexible as long as there's not too many lights featured at once.

--- drillbird/Scenes/Light.gd
+++ drillbird/Scenes/Light.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @export var lightTexture:GradientTexture2D
 @onready var lightSource=$light_full
-@export var minLight:float =0.15
+@export var minLight:float =0.1
 @export var maxLight:float = 0.9
+@onready var playerAvatar= $"../Player"
 
 var test:float =maxLight
 var dir:bool=false
@@ -14,12 +15,13 @@ var lightStrength:float=1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	SetLight(1)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	self.position=playerAvatar.position
 	pass
 
 func SetLight(lightstr):
@@ -27,15 +29,15 @@ func SetLight(lightstr):
 	var test =clamp(lightstr,minLight,maxLight)
 	
 	var a :float = maxLight*lightstr+minLight*(1-lightstr)
+	var b = clampf(a-0.3,0,a)
 	
 	var gradient_data := {
-	
-	1: Color.WHITE,
-	a: Color.WHITE,
-	0.1: Color.BLACK,
-	0: Color.BLACK,
-
+	1: Color.BLACK,
+	a: Color.BLACK,
+	b: Color.WHITE,
+	0.0: Color.WHITE,
 	}
+	
 	var gradient := Gradient.new()
 	gradient.offsets = gradient_data.keys()
 	gradient.colors = gradient_data.values()
