6f9d5675cc448421c42b6a2cb9b9ab4850e03549
Mnniska <slebbastian@gmail.com>
2024-12-19 10:32:26 +0100

    Set up health manager
    Health manager will now
    - read the amount of available health from globalvariables at setup and spawn the appropiate amount of hearts
    - deal damage and tell its hearts to change appearance accordingly
    - Set up logic to detect death, but needs to be hooked up to UI

--- drillbird/Scripts/HealthUIHandler.gd
+++ drillbird/Scripts/HealthUIHandler.gd
@@ -1,14 +1,71 @@
 extends Node2D
 
-var HealthArray:Array[Node2D]
+var HealthArray:Array[Node2D] #I'd love for this to specify the heart script. Make sure to loop up a tutorial over christmas :) 
 var HealthScene
+@export var HealthUpgrades:abstract_purchasable
+var HeartScene = preload("res://Scenes/HeartScene.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	HealthSetup()
 	pass # Replace with function body.
 
+func _process(delta:float)->void: 
 	
+	#debug test
+	if Input.is_action_just_pressed("removeLight"):
+		TakeDamage(1)
+	if Input.is_action_just_pressed("addLight"):
+		RefillHealth()
 	
+func HealthSetup():
+	
+	var HeartAmount:int = HealthUpgrades.items[GlobalVariables.upgradeLevel_health].power
+	var offset:int=11
+	
+	for n in HeartAmount:
+		var heartInstance=HeartScene.instantiate()
+		heartInstance.transform.origin+=Vector2(offset*n,0)
+		add_child(heartInstance)
+		HealthArray.append(heartInstance)
+		pass
+	
+	RefillHealth()
+	pass
+	
+func RefillHealth():
+	for n in HealthArray:
+		n.RefillHeart()
+
 func TakeDamage(amount:int):
-	#go through list of health and deal dmg to player
+
+	var DamageCounter:int=0
+	var index:int=0
+	var lastHeart:bool=false
+	
+	for n in HealthArray:
+		var c:int=HealthArray.size()-(index+1)
+		
+		if HealthArray[c].TakeDamage(): #Will return false if selected heart is empty
+			
+			lastHeart=index==HealthArray.size()-2
+			if lastHeart:
+				n.SetIsLastHeart()
+					
+			DamageCounter+=1
+			if DamageCounter>=amount: #continue removing health until specified amount has been taken
+				break
+		index+=1
+	
+	var survived:bool=false
+	for n in HealthArray:
+		if n.full:
+			survived=true
+	
+	if !survived:
+		print_debug("Player has DIED!")
+		pass
+		#Do death stuff!
+
 	pass
--- drillbird/Scripts/HeartScript.gd
+++ drillbird/Scripts/HeartScript.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 enum states {EMPTY,FULL,LASTFULL,AVAILABLE}
 var state=states.EMPTY
+var full:bool=true
+@onready var animator=$HealthAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +13,43 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func GetStateEnum():
+	return states
+
+func TakeDamage():
+	if full:
+		SetState(states.EMPTY)
+		full=false
+		return true
+	else:
+		return false
+
+func RefillHeart():
+	SetState(states.FULL)
+	full=true
+
+func SetIsLastHeart():
+	SetState(states.LASTFULL)
+
+func SetState(_state:states):
+	state=_state
+	var anim="empty"
+	match state:
+		states.EMPTY:
+			anim="empty"
+			
+		states.FULL:
+			anim="full"
+			
+		states.LASTFULL:
+			anim="full_last"
+			
+		states.AVAILABLE:
+			anim="available"
+	
+	animator.animation=anim
+	animator.play()
+	
 	pass
