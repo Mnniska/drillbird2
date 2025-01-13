06522ccf0c05db96b778a6a7043900d56110202e
Mnniska <slebbastian@gmail.com>
2025-01-13 12:03:46 +0100

    Changed Torches to be spawned in rather than hand placed

--- drillbird/Scripts/enemy_spawner.gd
+++ drillbird/Scripts/enemy_spawner.gd
@@ -1,8 +1,9 @@
 extends Node2D
 @export var potentialEnemyStrings:Array[String]
+@export var potentialObjectStrings:Array[String]
 
 
-@onready var enemyTilemap:TileMapLayer=$"../Tilemap_Enemies"
+@onready var EnemyObjectTilemap:TileMapLayer=$"../Tilemap_EnemiesAndObjects"
 
 
 
@@ -29,8 +30,11 @@ func GameStart():
 
 
 	SpawnAllEnemies()
-	enemyTilemap.hide()
+	
+	GenerateObjectsFromTilemap()
+	
 	#enemyTilemap.hide()
+
 	pass
 
 func LoadEnemySpawns(spawnpos:Array[Vector2i],enemytype:Array[int]):
@@ -43,20 +47,45 @@ func LoadEnemySpawns(spawnpos:Array[Vector2i],enemytype:Array[int]):
 		enemiesToSpawnList.append(enemy)
 	
 	pass
+	
+func GenerateObjectsFromTilemap():
+	var ObjectSpawnLocations = EnemyObjectTilemap.get_used_cells()
+
+	for n in ObjectSpawnLocations.size():
+		var tile:TileData = EnemyObjectTilemap.get_cell_tile_data(ObjectSpawnLocations[n])
+		var type=tile.get_custom_data("object_type")
+		if !type<=0:
+			
+			var object = load(potentialObjectStrings[type-1]) 
+			var node = object.instantiate()
+
+			var globalSpawnPos= EnemyObjectTilemap.map_to_local(ObjectSpawnLocations[n]) 
+			var localSpawnPos = self.to_local(globalSpawnPos)
+			
+			
+			node.transform.origin = localSpawnPos
+			add_child(node)
+			
+			
+			pass
+	
+	pass
 
 func GenerateEnemySpawnsFromTilemap():
 	#Should only be done if there is no save data
 	#Creates a new abstract_enemy per spawn location and adds it to a list
 	enemiesToSpawnList.clear()
-	var enemySpawnLocations = enemyTilemap.get_used_cells()
+	var enemySpawnLocations = EnemyObjectTilemap.get_used_cells()
 
 	var index=0
 	for n in enemySpawnLocations:
-		var tile:TileData = enemyTilemap.get_cell_tile_data(enemySpawnLocations[index])
-		var newEnemy=abstract_enemy.new()
-		newEnemy.type=tile.get_custom_data("enemy_type")
-		newEnemy.spawnLocation=enemySpawnLocations[index]
-		enemiesToSpawnList.append(newEnemy)
+		var tile:TileData = EnemyObjectTilemap.get_cell_tile_data(enemySpawnLocations[index])
+		
+		if !tile.get_custom_data("enemy_type")==0: #zero is default value, meaning this is not an enemy
+			var newEnemy=abstract_enemy.new()
+			newEnemy.type=tile.get_custom_data("enemy_type")
+			newEnemy.spawnLocation=enemySpawnLocations[index]
+			enemiesToSpawnList.append(newEnemy)
 		index+=1
 			
 	return enemiesToSpawnList
@@ -69,12 +98,12 @@ func SpawnAllEnemies():
 	var index=0
 	for n in enemiesToSpawnList:
 		
-		var enemy = load(potentialEnemyStrings[enemiesToSpawnList[index].type])
+		var enemy = load(potentialEnemyStrings[enemiesToSpawnList[index].type-1]) #The -1 is due to 0 acting as there being NO enemy
 		var node = enemy.instantiate()
 		spawnedEnemies.append(node)	
 		print_debug("Spawnpos: "+str(enemiesToSpawnList[index].spawnLocation))
 
-		var globalSpawnPos= enemyTilemap.map_to_local(enemiesToSpawnList[index].spawnLocation) 
+		var globalSpawnPos= EnemyObjectTilemap.map_to_local(enemiesToSpawnList[index].spawnLocation) 
 		var localSpawnPos = self.to_local(globalSpawnPos)
 		
 		
@@ -95,8 +124,8 @@ func UpdateEnemySpawnLocations():
 		var spawnpos2:Vector2i=n.GetSpawnPosition()
 		
 		
-		var spawnpos:Vector2i=enemyTilemap.local_to_map(enemyTilemap.to_local(n.GetSpawnPosition()))
-		var newpos:Vector2i=enemyTilemap.local_to_map(enemyTilemap.to_local(n.position))
+		var spawnpos:Vector2i=EnemyObjectTilemap.local_to_map(EnemyObjectTilemap.to_local(n.GetSpawnPosition()))
+		var newpos:Vector2i=EnemyObjectTilemap.local_to_map(EnemyObjectTilemap.to_local(n.position))
 		#Convert spawnpos to tilemap coordinates 
 		
 		if spawnpos!=newpos:
@@ -116,12 +145,12 @@ func UpdateEnemySpawnLocations():
 func MoveTileToNewPos(oldpos:Vector2,newpos:Vector2):
 	var tile_map_layer = 0 
 	var tile_map_cell_position = oldpos 
-	var tile_data = enemyTilemap.get_cell_tile_data(tile_map_cell_position)
+	var tile_data = EnemyObjectTilemap.get_cell_tile_data(tile_map_cell_position)
 	if tile_data: 
-		var tile_map_cell_source_id = enemyTilemap.get_cell_source_id(tile_map_cell_position); 
-		var tile_map_cell_atlas_coords = enemyTilemap.get_cell_atlas_coords(tile_map_cell_position) 
-		var tile_map_cell_alternative = enemyTilemap.get_cell_alternative_tile(tile_map_cell_position) 
+		var tile_map_cell_source_id = EnemyObjectTilemap.get_cell_source_id(tile_map_cell_position); 
+		var tile_map_cell_atlas_coords = EnemyObjectTilemap.get_cell_atlas_coords(tile_map_cell_position) 
+		var tile_map_cell_alternative = EnemyObjectTilemap.get_cell_alternative_tile(tile_map_cell_position) 
 		var new_tile_map_cell_position = newpos
-		enemyTilemap.set_cell(new_tile_map_cell_position, tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
+		EnemyObjectTilemap.set_cell(new_tile_map_cell_position, tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
 			
 		
