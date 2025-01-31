42562c1bf0983e64f5f8e5113c0323a554d2e6c0
Mnniska <slebbastian@gmail.com>
2025-01-31 18:16:12 +0100

    Fixed bug where pitch would not reset correctly when drilling air or solid

--- drillbird/Scripts/DrillSoundScript.gd
+++ drillbird/Scripts/DrillSoundScript.gd
@@ -40,6 +40,7 @@ func MaterialChange(terrain:abstract_terrain_info):
 	
 	if terrain==null: #This has not yet been implemented - but I think we should in the future
 		if !audiostream.playing or audiostream.stream!=sound_air:
+			audiostream.pitch_scale=0
 			SetupAndPlaySoundSetting(sound_air)
 		return
 
@@ -50,6 +51,7 @@ func MaterialChange(terrain:abstract_terrain_info):
 		
 	
 	if terrain.terrainIdentifier<=0: #Solid
+		audiostream.pitch_scale=0
 		SetupAndPlaySoundSetting(sound_solid)
 
 	
