f4c9c796c6b7556835ef057f9724c51e1a9fe44d
Mnniska <slebbastian@gmail.com>
2025-02-05 18:14:23 +0100

    Added upside_down enemy

--- drillbird/Scripts/Abstract scripts/abstract_enemy.gd
+++ drillbird/Scripts/Abstract scripts/abstract_enemy.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name abstract_enemy
 
-enum enemyTypes{MOLE,SPIKE,FOLLOWER,FALLBLOCK}
+enum enemyTypes{MOLE,SPIKE,FOLLOWER,FALLBLOCK,UPSIDEDOWN}
 @export var type:enemyTypes
 var spawnLocation:Vector2i
 var dead:bool=false
---
+++ drillbird/Scripts/Enemies/enemy_upside_down.gd
@@ -0,0 +1,57 @@
+extends Base_Enemy
+
+enum States{WALK,WAIT}
+var state:States=States.WALK
+var direction:float=1
+const SPEED = 40.0
+var positionLastFrame:Vector2
+
+@export var timeInWait=1
+var waitCounter=0
+
+func _physics_process(delta: float) -> void:
+	
+	if enemyInfo.dead:
+		return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += -get_gravity() * delta
+
+	if state==States.WALK:
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var anim="idle"
+	
+	if state==States.WALK:
+		anim="run"
+
+
+	move_and_slide()
+	UpdateAnimations(anim)
+
+	if state==States.WALK:
+		
+		var speed = positionLastFrame-position
+		if speed.x==0:
+
+			direction=direction*-1
+			state=States.WAIT
+		positionLastFrame=position
+	
+
+	if state==States.WAIT:
+		waitCounter+=delta
+		if waitCounter>timeInWait:
+			waitCounter=0
+			state=States.WALK
+
+func UpdateAnimations(_anim:String):
+
+	anim.animation=_anim
+	anim.flip_h=direction <0
+
+	pass
