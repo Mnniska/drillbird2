ff106d9f980d34fcaa6ca09c84baa00aa0c3f478
Mnniska <slebbastian@gmail.com>
2025-03-12 15:43:25 +0100

    Fixed issue where spike blocks would not remove their collision

--- drillbird/Scripts/Enemies/Enemy_Spikes.gd
+++ drillbird/Scripts/Enemies/Enemy_Spikes.gd
@@ -41,6 +41,7 @@ func SpikesSpawnSetup():
 func TurnEnemyOff():
 	hide()
 	$".".set_deferred("disabled",true)
+	$SpikeAffectedCollider.set_deferred("disabled",true)
 	$Collider.set_deferred("disabled",true)
 	$Collider.set_deferred("monitoring",false)
 	enemyInfo.dead=true	
