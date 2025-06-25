c6088f2f2f166fb0bb9d6ff5e924a24205778b08
Mnniska <slebbastian@gmail.com>
2025-06-25 15:53:11 +0200

    Fonts are not converted to binary to avoid remapping issue

--- drillbird/Scripts/UX and UI/options_screen.gd
+++ drillbird/Scripts/UX and UI/options_screen.gd
@@ -226,6 +226,7 @@ func ButtonPressed(_option:menu_option):
 
 func SetLanguage(lang:String):
 	TranslationServer.set_locale(lang)
+
 	UpdateMenu()
 	GlobalVariables.MainSceneReferenceConnector.shop.UpdateShop()
 	
