e24e753c9cade1d2c54c0f1c0e473c5d99fa62f3
Mnniska <slebbastian@gmail.com>
2026-05-09 21:15:45 +0200

    added dialogue to demon lair

---
+++ drillbird/Scripts/cutscenes/dialogue_script.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+signal dialogueFinished
+
+@onready var dialogueContainter=$VBoxContainer
+@onready var text:RichTextLabel=$VBoxContainer/PanelContainer/MarginContainer/text
+@export var linesToPlay:Array[String]
+#could also make a dialogue script and have an array of those if we want same dialogue script to alter betweeen diff dialogues
+
+var dialogueHasPlayed:bool=false
+
+var currentLineIndex:int=0
+
+func _ready() -> void:
+	hide()
+
+func StartDialogue():
+	dialogueHasPlayed=true
+	currentLineIndex=0
+	show()
+	
+	ContinueDialogue()
+	pass
+
+
+func ContinueDialogue():
+	text.text=tr(linesToPlay[currentLineIndex])
+	
+	await GlobalVariables.playerSang
+	
+	if currentLineIndex<linesToPlay.size()-1:
+		currentLineIndex+=1
+		ContinueDialogue()
+	else:
+		hide()
+		dialogueFinished.emit()
+	pass
+
+
+func _on_player_collider_body_entered(body: Node2D) -> void:
+	
+	if !dialogueHasPlayed:
+		StartDialogue()
+	
+	pass # Replace with function body.
