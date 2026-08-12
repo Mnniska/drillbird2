fce61fe0a72470962ac49968f4cad4104e43ce12
Mnniska <slebbastian@gmail.com>
2026-08-12 15:37:49 +0200

    Added some sick SAINT ANIMATIONS to their dialogue

--- drillbird/Scripts/cutscenes/dialogue_script.gd
+++ drillbird/Scripts/cutscenes/dialogue_script.gd
@@ -15,6 +15,10 @@ signal aboutToPlay
 @onready var continueSymbolParent:HBoxContainer=$HBoxContainer
 @onready var continueSymbolText:RichTextLabel=$HBoxContainer/iconHolder/text_icon
 
+@export var saintTalking:bool=false
+
+@export var maxBoxWidth:int=220
+
 #could also make a dialogue script and have an array of those if we want same dialogue script to alter betweeen diff dialogues
 
 var dialogueHasPlayed:bool=false
@@ -29,8 +33,25 @@ var waitingForPlayer:bool=false
 var counterBeforeShowingButton:float=0
 
 func _ready() -> void:
+	SetupDialogueBoxSettings(saintTalking)
 	hide()
+	
 
+func SetupDialogueBoxSettings(isSaint:bool):
+	
+	
+	if isSaint:
+		$tail.position=$tailLeftPosition.position
+		$tail.flip_h=true
+		$VBoxContainer/HBoxContainer.alignment=0
+		maxBoxWidth=150
+	else:
+		$tail.position=$tailCenterPosition.position
+		$tail.flip_h=false
+		$VBoxContainer/HBoxContainer.alignment=1
+		maxBoxWidth=220
+	
+	
 
 func StartDialogue():
 	aboutToPlay.emit()
@@ -50,25 +71,50 @@ func _process(delta: float) -> void:
 
 
 func SetShowContinueButton(show:bool):
-	if show:
+	if show and !currentLineIsOnlyAnim:
 		continueSymbolParent.show()
 	else:
 		continueSymbolParent.hide()
+		
+var currentLineIsOnlyAnim:bool=false
 
 func ContinueDialogue():
-	if currentLineIndex==0:
+	
+	var _text=null
+	if linesToPlay[currentLineIndex].lineToPlay!="":
+		_text=tr(linesToPlay[currentLineIndex].lineToPlay) %str(GlobalVariables.daysBeforeDemonKillsEgg- GlobalVariables.currentDay)
+		text.text=_text
+		currentLineIsOnlyAnim=false
+	else:
+		currentLineIsOnlyAnim=true
+		text.text=""
+	
+	if currentLineIndex==0 and !currentLineIsOnlyAnim:
 		SetShowContinueButton(true)
 	else:
 		SetShowContinueButton(false)
 	counterBeforeShowingButton=0
 	
-	var _text=tr(linesToPlay[currentLineIndex].lineToPlay) %str(GlobalVariables.daysBeforeDemonKillsEgg- GlobalVariables.currentDay)
-	text.text=_text
-	var boxSize=Vector2(min(220,_text.length()*6),0)
+
 	
-	#Set continue button to right side if dialogue box, it changes size depending on spacing
-	$VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/text.custom_minimum_size= boxSize
-	$HBoxContainer.position.x=boxSize.x/2
+	
+	if currentLineIsOnlyAnim: #leaving a text empty allows animator to play only animations without dialogues
+		$VBoxContainer.hide()
+		$tail.hide()
+	else:
+		$VBoxContainer.show()
+		$tail.show()
+		#maxbozxwidth changes depending on if saint or demon is talking
+		var boxSize=Vector2(min(maxBoxWidth,_text.length()*6),0)
+	
+	
+		#Set continue button to right side if dialogue box, it changes size depending on spacing
+		$VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/text.custom_minimum_size= boxSize
+	
+		if saintTalking:
+			$HBoxContainer.position.x=$tail.position.x + boxSize.x - 10
+		else:
+			$HBoxContainer.position.x=boxSize.x/2
 	
 	if linesToPlay[currentLineIndex].soundToPlay:
 		soundPlayer.stream=linesToPlay[currentLineIndex].soundToPlay
@@ -79,13 +125,16 @@ func ContinueDialogue():
 	#Animation stuff 
 	if linesToPlay[currentLineIndex].animToPlay!=null and animatorToAnimate!=null:
 		animatorToAnimate.animation=linesToPlay[currentLineIndex].animToPlay
+		animatorToAnimate.play()
 	
 	#sound
 	if linesToPlay[currentLineIndex].soundToPlay!=null:
 		pass
-		
 	
-	await GlobalVariables.playerSang
+	if !currentLineIsOnlyAnim:
+		await GlobalVariables.playerSang
+	else:
+		await animatorToAnimate.animation_finished
 	
 	if currentLineIndex<linesToPlay.size()-1:
 		currentLineIndex+=1
